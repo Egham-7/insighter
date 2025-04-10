034de835ce3393c395101716bdf84d8809aec0f6
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-04-10 22:37:38 +0100

    tested text generation and streaming with ai agents

--- mastra/index.ts
+++ mastra/index.ts
@@ -1,13 +1,53 @@
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
 
-import { Mastra } from '@mastra/core/mastra';
-import { createLogger } from '@mastra/core/logger';
-
-import { weatherAgent } from './agents';
+import { weatherAgent } from "./agents";
+import { myAgent } from "./agents";
 
 export const mastra = new Mastra({
-  agents: { weatherAgent },
+  agents: { weatherAgent, myAgent },
   logger: createLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info",
   }),
 });
+
+// Generating Text
+const response = await myAgent.generate([
+  { role: "user", content: "Hello, how can you assist me today?" },
+]);
+
+console.log("Agent:", response.text);
+
+// Test Agent
+async function testAgent() {
+  try {
+    const response = await myAgent.generate([
+      { role: "user", content: "Test!" },
+    ]);
+    console.log("Agent:", response.text);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+//testAgent();
+
+async function testStreaming() {
+  try {
+    const stream = await myAgent.stream([
+      {
+        role: "user",
+        content:
+          "Tell me how to best use agents to analyze business csv data to generate them a report of charts and insigghts that will grow their business.",
+      },
+    ]);
+    console.log("Agent:");
+    for await (const chunk of stream.textStream) {
+      process.stdout.write(chunk);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+testStreaming();
