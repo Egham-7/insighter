b946e54e58d1fadea933bd1a21301bc9ed0a503c
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-04-11 19:55:05 +0100

    Remove myAgent from Mastra agent configuration

--- mastra/index.ts
+++ mastra/index.ts
@@ -1,10 +1,9 @@
 import { Mastra } from "@mastra/core/mastra";
 import { createLogger } from "@mastra/core/logger";
 import { dataAnalystAgent4o } from "./agents";
-import { myAgent } from "./agents";
 
 export const mastra = new Mastra({
-  agents: { myAgent, dataAnalystAgent4o },
+  agents: { dataAnalystAgent4o },
   logger: createLogger({
     name: "Mastra",
     level: "info",
